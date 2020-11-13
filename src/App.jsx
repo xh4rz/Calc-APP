491042139e52aa7e47e3646718be9c60d396d660
xh4rz <xh4rz@hotmail.com>
2020-11-13 02:33:51 -0500

    Subiendo los archivos calculadora

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+/* eslint no-eval: 0*/
+import React, { useState } from 'react'
+import words from 'lodash.words'
+import Functions from './components/Functions'
+import Numbers from './components/Numbers'
+import MathOperations from './components/MathOperations'
+import Result from './components/Result'
+import './App.css'
+
+const App = () => {
+
+    const [stack, setStack] = useState("")
+
+    const items = words(stack, /[^-^+^*^/]+/g)
+
+    const value = items.length > 0 ? items[items.length-1] : "0";
+
+    return (
+        <main className='react-calculator'>
+            <Result value={value}/>
+            <Numbers onClickNumber={number =>  setStack(`${stack}${number}`)}/>
+            <Functions
+             onContentClear={() => setStack('')}
+             onDelete={() => {
+                if(stack.length> 0) {
+                    const newStack = stack.substring(0, stack.length - 1)
+                    setStack(newStack)
+                    }
+                }}
+            />
+            <MathOperations
+            onClickOperation={operation => setStack(`${stack}${operation}`)}
+            onClickEqual={equal => setStack(eval(stack).toString())}
+            />
+        </main>)
+}
+
+export default App
---
+++ src/components/Numbers.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import Button from './Button'
+
+const numbers = [7, 8, 9, 4, 5, 6, 1, 2, 3, 0]
+
+const renderButtons = onClickNumber => {
+    
+    const renderButton = number => (
+        <Button
+             key={number}
+             text= {number.toString()}
+             clickHandler={onClickNumber}
+        />
+    )
+    return numbers.map(renderButton)
+}
+
+const Numbers = ({onClickNumber}) => (
+    <section className="numbers">
+        {
+            renderButtons(onClickNumber)
+        }
+    </section>
+)
+
+Number.propTypes = {
+    onClickNumber: PropTypes.func.isRequired,
+}
+
+export default Numbers
